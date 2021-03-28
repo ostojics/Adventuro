fab63872365d21aa02fdceb356442fc4390ab913
ostojics <ostojic.dev@gmail.com>
2021-03-28 22:06:18 +0200

    Added improvements to the homepage

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 
 import React, { Component } from 'react';
+import { Route } from 'react-router-dom';
 import Home from './containers/Home/Home';
+import Camps from './containers/Camps/Camps';
+
 
 class App extends Component {
 
 render() {
     return (
-      <div className="App">
-        <Home />
-      </div>
+        <div className="App">
+          <Route path = '/' exact component = { Home }/>
+          <Route path = '/camps' component = { Camps } />
+        </div>
     );
 }
  
--- src/components/Options/Option/Option.js
+++ src/components/Options/Option/Option.js
@@ -1,16 +1,30 @@
 
-import React from 'react';
+import React, { Component } from 'react';
+import styled from 'styled-components';
 import './Option.scss';
 import {ReactComponent as KayakIcon} from '../../../icons/kayak.svg';
 import {ReactComponent as CampIcon} from '../../../icons/tent.svg';
 import {ReactComponent as BikeIcon} from '../../../icons/road-bike.svg';
 import {ReactComponent as MountainIcon} from '../../../icons/mountains.svg';
 
-const Option = props => {
+
+
+class Option extends Component {
+    state = {
+        clicked: false
+    }
+
+    optionCardStyleHandler = () => {
+        const currentState = this.state.clicked; 
+        this.setState({clicked: !currentState})
+    }
+
+
+    render() {
 
     let campIcon = null;
 
-    switch(props.title) {
+    switch(this.props.title) {
         case('Camping'):
             campIcon = <CampIcon className = { 'Icon' }/>
             break;
@@ -28,14 +42,27 @@ const Option = props => {
             break;
     }
 
-    return (
-        <div className = { 'option' }>
-             <div className = { 'option-card' }>
-                { campIcon }
+    const StyledOptionCard = styled.div`
+        padding: 1.25em;
+        background-color: #181F38;
+        border-radius: 1.25em;
+        border: ${props => props.clicked ? '5px solid #EF6E6F' : '5px solid transparent'};
+        transition: all .2s;
+        cursor: pointer;
+        &:hover {
+            border: ${props => props.clicked ? '5px solid #EF6E6F' : '5px solid #6573B5'};
+        }
+    `;
+
+        return (
+            <div className = { 'option' }>
+                <StyledOptionCard clicked = { this.state.clicked } onClick = { this.optionCardStyleHandler }>
+                    { campIcon }
+                </StyledOptionCard>
+                <p className = { 'option-text' }>{ this.props.title }</p>
             </div>
-            <p className = { 'option-text' }>{ props.title }</p>
-        </div>
-    )
+        )
+    }    
 }
 
 export default Option;
