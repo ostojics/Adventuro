9f6c665099fd49527e0dbeefda8043730bec7e37
ostojics <ostojic.dev@gmail.com>
2021-04-13 22:33:18 +0200

    Started working on the authentication, added the ability to sign up and to sign in

--- src/containers/Auth/Auth.js
+++ src/containers/Auth/Auth.js
@@ -1,8 +1,11 @@
 import React, { Component } from 'react';
 import './Auth.scss';
+import { connect } from 'react-redux';
 
 import AuthInput from '../../components/UI/AuthInput/AuthInput';
 import AuthButton from '../../components/UI/AuthButton/AuthButton';
+import * as actions from '../../store/actions/index';
+import Loader from '../../components/UI/Loader/Loader';
 
 class Auth extends Component {
     state = {
@@ -98,7 +101,7 @@ class Auth extends Component {
         },
         signUpValid: false,
         signInValid: false,
-        currentMethod: 'signUp'
+        currentMethod: 'signIn'
     }
 
     methodChangeHandler = () => {
@@ -179,6 +182,16 @@ class Auth extends Component {
         }
     }
 
+    submitHandler = (event) => {
+        event.preventDefault();
+        let currentForm = null;
+        this.state.currentMethod === 'signIn' ? currentForm = this.state.signInForm : currentForm = this.state.signUpForm;
+        this.props.onAuth(currentForm['email'].value, currentForm['password'].value, this.state.currentMethod);
+        if(this.state.currentMethod === 'signUp') {
+            this.methodChangeHandler();
+        }
+    }
+
 
     render() {
 
@@ -199,42 +212,52 @@ class Auth extends Component {
             })
         }
 
+    
         let form = (
-            <div className='form-wrapper'>
-                 <form className='AuthForm text-center'>
-                        <h2 className='AuthForm-title text-center'>Adventuro</h2>
-                    { formElements.map(formElement => {
-                    return (
-                        <div className='AuthForm-group' key = { formElement.id }>
-                            <AuthInput 
-                            label = { formElement.config.elementConfig.label }
-                            type = { formElement.config.elementConfig.elementType }
-                            placeholder = { formElement.config.elementConfig.placeholder }
-                            name = { formElement.config.elementConfig.name }
-                            value = { formElement.config.value }
-                            changed = { (event) => this.onChangeHandler(event, formElement.id) }
-                            errorMsg = { formElement.config.elementConfig.errorMessage }
-                            touched = { formElement.config.touched }
-                            valid = { formElement.config.valid }
-                            rule = { formElement.config.elementConfig.rule }
-                            validMsg = { formElement.config.elementConfig.validMsg } />
-                        </div>
-                    )
-                    }) }
-                        { this.state.currentMethod === 'signUp' ? (
-                            <p className='AuthForm-switcher'>Already have an account? <span onClick={ this.methodChangeHandler }>Sign in</span></p>
-                        ) : (
-                            <p className='AuthForm-switcher'>Don't have an account? <span onClick={ this.methodChangeHandler }>Sign up</span></p>
-                        ) }
+            <form className='AuthForm text-center' onSubmit={ this.submitHandler }>
+                    <h2 className='AuthForm-title text-center'>Adventuro</h2>
+                { formElements.map(formElement => {
+                return (
+                    <div className='AuthForm-group' key = { formElement.id }>
+                        <AuthInput 
+                        label = { formElement.config.elementConfig.label }
+                        type = { formElement.config.elementConfig.elementType }
+                        placeholder = { formElement.config.elementConfig.placeholder }
+                        name = { formElement.config.elementConfig.name }
+                        value = { formElement.config.value }
+                        changed = { (event) => this.onChangeHandler(event, formElement.id) }
+                        errorMsg = { formElement.config.elementConfig.errorMessage }
+                        touched = { formElement.config.touched }
+                        valid = { formElement.config.valid }
+                        rule = { formElement.config.elementConfig.rule }
+                        validMsg = { formElement.config.elementConfig.validMsg } />
+                    </div>
+                )
+                }) }
+                    { this.state.currentMethod === 'signUp' ? (
+                        <p className='AuthForm-switcher'>Already have an account? <span onClick={ this.methodChangeHandler }>Sign in</span></p>
+                    ) : (
+                        <p className='AuthForm-switcher'>Don't have an account? <span onClick={ this.methodChangeHandler }>Sign up</span></p>
+                    ) }
 
-                        { this.state.currentMethod === 'signUp' ? (
-                            <AuthButton disabled = { !this.state.signUpValid }>SIGN UP</AuthButton>
-                        ) : (
-                            <AuthButton>SIGN IN</AuthButton>
-                        ) }
-                </form>
-            </div>
+                    { this.state.currentMethod === 'signUp' ? (
+                        <AuthButton disabled = { !this.state.signUpValid }>SIGN UP</AuthButton>
+                    ) : (
+                        <AuthButton>SIGN IN</AuthButton>
+                    ) }
+             </form>
         )
+
+        if(this.props.loading) {
+            form = <Loader/>
+        }
+
+        let errorMessage = null;
+        if(this.props.error) {
+            errorMessage = (
+                <p>{ this.props.error.message }</p>
+            )
+        }
 
         return (
             <section className='section-auth'>
@@ -245,11 +268,27 @@ class Auth extends Component {
                             <p>Holidays full of adventures are waiting for you</p>
                         </div>
                     </div>
-                    { form }
+                    <div className='form-wrapper'>
+                        { form }
+                        { errorMessage }
+                    </div>
                 </div>
             </section>
         )
     }
 }
 
-export default Auth;
+const mapStateToProps = state => {
+    return {
+        loading: state.auth.loading,
+        error: state.auth.error
+    }
+}
+
+const mapDispatchToProps = dispatch => {
+    return {
+        onAuth: (email, password, method) => dispatch(actions.auth(email, password, method))
+    }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Auth);
